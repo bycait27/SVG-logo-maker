90f6c05d623b211b802bbcef71277cb8e849a5c9
Caitlin Ash <caitlinba27@gmail.com>
2023-08-24 15:51:08 -0500

    Added test file for Circle class

---
+++ tests/circle.test.js
@@ -0,0 +1,12 @@
+const Circle = require('/Users/caitlinash/Desktop/coding-challenges/SVG-logo-maker/lib/circle.js');
+
+describe('Cricle', () => {
+    test('should render a logo in the shape of a circle.', () => {
+        const expectedLogo = [
+            `<circle cx="50" cy="75" r="50" fill="${this.shapeColor}"/>`,
+            `<text x="50" y="90" font-size="45" text-anchor="middle" fill="${this.textColor}">${this.characters}</text>`,
+        ].join('');
+        const circle = new Circle().render();
+        expect(circle).toEqual(expectedLogo);
+    })
+})
